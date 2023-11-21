0deed3c7b244cb5fa59201232cd1c85a637a2477
621352 <camarenburt>
2023-11-21 09:43:44 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String s;
+        String current;
+        String current2 = "";
+        System.out.println("Please enter a word or phrase.");
+        s = scan.nextLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            current = s.substring(i, i+1);
+            if (i == 0 || !s.substring(0,i).contains(current) || current.equals(" ")){
+                current2 = current2 + current;
+            }
+        }
+        System.out.println(current2);
     }
 }
+
